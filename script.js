dda2559006c9eb01be04a7a3c38a399b5feb6dc8
Serena-Dong <serenadong3159@icloud.com>
2022-12-21 14:12:50 +0100

    .

---
+++ script.js
@@ -0,0 +1,39 @@
+//ELEMENTI DEL DOM
+const daysCount = document.getElementById('days');
+const hoursCount = document.getElementById('hours');
+const minutesCount = document.getElementById('minutes');
+const secondsCount = document.getElementById('seconds');
+
+function timeLeft() {
+//Data di oggi
+const now = new Date()
+const msNow = now.getTime();
+
+//Date di natale
+const xmasDate = new Date ('12-25-2022');
+const msXmasDate = xmasDate.getTime ();
+
+//Quanto manca
+const msLeft = msXmasDate - msNow;
+
+//Conversione
+let secondsLeft = Math.floor(msLeft / 1000);
+let minutesLeft = Math.floor(secondsLeft / 60);
+let hoursLeft = Math.floor(minutesLeft / 60);
+let daysLeft = Math.floor(hoursLeft / 24);
+
+secondsLeft = secondsLeft % 60;
+minutesLeft = minutesLeft % 60;
+hoursLeft = hoursLeft % 24;
+
+//Stampo in pagina
+daysCount.innerText = daysLeft;
+hoursCount.innerText = hoursLeft;
+minutesCount.innerText = minutesLeft;
+secondsCount.innerText = secondsLeft;  
+
+};
+
+timeLeft();
+
+setInterval (timeLeft, 1000);
